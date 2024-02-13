58f525353bca3f4322449691725a1d812fd5cace
SolenPB <solene.paratre@gmail.com>
2024-02-13 10:18:46 +0100

    add nav

---
+++ ux-portfolio/src/components/NavLink.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+	name: 'NavLink',
+	props: ['url', 'text']
+}
+</script>
+
+<template>
+	<a :href="url">{{ text }}</a>
+</template>
+<style>
+    a{
+        color: blue;
+        text-decoration: none;
+    }
+</style>
