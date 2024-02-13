78299d3a9f67f885900a1d3feefb5683491cd16c
SolenPB <solene.paratre@gmail.com>
2024-02-13 15:46:42 +0100

    complétion de la page à propos

--- ux-portfolio/src/views/AboutView.vue
+++ ux-portfolio/src/views/AboutView.vue
@@ -1,5 +1,15 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Qui suis-je ?</h1>
+    <p>
+      Pour faire simple, je viens des sciences humaines: plus précisément de la psychologie. 
+      Je n'ai malheureusement pu finir mon parcours. 
+      Après plusieurs années dans le commerce, j'ai rebondi une première fois grâce à une formation de conseiller
+      en évolution professionnelle. Ce qui m'a donné l'opportunité d'exercer en centre de formation, pour accompagner et conseiller
+      des personnes dans l'élaboration et la réalisation de projets professionnels stables et durables. 
+      <br>
+      C'est en faisant intervenir un centre de formation du numérique que j'ai découvert l'UX design. J'ai vu en cette discipline
+      ce que je cherchais à faire en psychologie: allier le bien-être d'autrui au numérique. 
+    </p>
   </div>
 </template>
