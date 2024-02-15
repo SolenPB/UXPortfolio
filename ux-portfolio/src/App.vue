5ad30861d4b503f03b795f00adab4b9619cf34af
SolenPB <solene.paratre@gmail.com>
2024-02-15 23:20:51 +0100

    maj

--- ux-portfolio/src/App.vue
+++ ux-portfolio/src/App.vue
@@ -1,10 +1,13 @@
 <template>
   <nav>
-    <router-link to="/">Accueil</router-link> |
-    <router-link to="/about">À propos</router-link> |
+    <router-link to="/">Accueil</router-link> 
+    <router-link to="/about">À propos</router-link> 
     <router-link to="/gallery">Portfolio</router-link>
   </nav>
   <router-view/>
+  <footer>
+    Hello ! Mon footer très prochainement !
+  </footer>
 </template>
 
 <style lang="scss">
@@ -21,6 +24,7 @@ nav {
   a {
     font-weight: bold;
     color: #2c3e50;
+    margin-left: 30px;
 
     &.router-link-exact-active {
       color: blue;
